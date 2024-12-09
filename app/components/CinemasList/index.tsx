a3e2a146497495e1273e3d6dec2ce94386e9a66c
Agnar Davíð Halldórsson <agnardavid@hotmail.com>
2024-12-09 14:35:23 +0000

    fix react issue

--- app/components/CinemasList/index.tsx
+++ app/components/CinemasList/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
+import { ReactNode } from "react";
 
-const CinemasList: React.FC<{ navigation: any }> = ({ navigation }) => {
+
+// instead of : React.FC<{ navigation: any }> = ({ navigation }) => 
+const CinemasList = ({ navigation }: { navigation: any }) => {
 
   return (
       <View style={styles.container}>
