de8e5b907e9e08fa23d4024fcde4d651afb93c3f
Julia <juliat22@ru.is>
2024-12-10 13:46:09 +0000

    routes...

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CinemasListView from '../views/CinemasList';
 import CinemasDetailView from '../views/CinemasDetail';
-import { NavigationContainer } from '@react-navigation/native';
 
 export type RootStackParamList = {
     CinemasList: undefined;
     CinemaDetail: undefined;
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
-const Routes = () => {
+const Routes: React.FC = () => {
     return (
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="CinemasList">
-          
-          <Stack.Screen name="CinemasList" component={CinemasListView} />
-          <Stack.Screen name="CinemaDetail" component={CinemasDetailView} />
+      <Stack.Navigator
+        initialRouteName="CinemasList">
+        
+        <Stack.Screen name="CinemasList" component={CinemasListView} />
+        <Stack.Screen name="CinemaDetail" component={CinemasDetailView} />
 
-        </Stack.Navigator>
-      </NavigationContainer>
+      </Stack.Navigator>
   );
 };
 
